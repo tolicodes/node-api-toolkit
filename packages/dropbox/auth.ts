8578954b38b29941c0da314e1aea1cd8a730d1cd
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-03-01 12:01:07 +0800

    Dropbox Oauth

--- packages/dropbox/auth.ts
+++ packages/dropbox/auth.ts
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express = require("express");
 import passport = require("passport");
 import { Strategy as DropboxOAuth2Strategy } from "passport-dropbox-oauth2";
 import url = require("url");
@@ -10,7 +10,21 @@ export type OnOauthSuccessOpts = {
   userId: string;
 };
 
-export type OnOauthSuccess = (OnOauthSuccessOpts) => Promise<void>;
+export type User = {
+  userId: string;
+  email: string;
+  name: {
+    givenName: string;
+    familyName: string;
+    displayName: string;
+  };
+  // any other user details
+  profile: object;
+  // possible metadata that will be attached by the onOauthSuccess callback
+  metadata?: object;
+};
+
+export type OnOauthSuccess = (OnOauthSuccessOpts) => Promise<User>;
 
 export type AuthOpts = {
   appKey: string;
@@ -30,13 +44,13 @@ export type AuthOpts = {
  *
  * 1. First passport is set up using the `appKey` and `appSecret`.
  * 2. Then when the user visits `authStartPath`, they are redirected to
- * Dropbox's server.
+ * Dropbox's oauth server.
  * 3. The user then logs in on Dropbox's server
  * 4. The Dropbox server responds with the authentication code to `callbackURL`
- * 5. Passport makes a request for the Token to the Dropbox API  using `appKey`
- * and `appSecret` and the `code` it just got back.
- * 6. If the token is fetched successful, we redirect to `successRedirect`, otherwise
- * we redirect to `failureRedirect`
+ * 5. Passport makes a request for the Token and Profile to the Dropbox API
+ * using `appKey`, `appSecret` and the `code` it just got back.
+ * 6. If the token and profile is fetched successful, we redirect to
+ * `successRedirect`, otherwise we redirect to `failureRedirect`
  *
  * @param appKey This is the app key/clientID you get from creating your Dropbox application
  * (https://docs.gravityforms.com/creating-a-custom-dropbox-app/) (ex: 3u23809sd90239)
@@ -44,7 +58,7 @@ export type AuthOpts = {
  * (ex: 3u23809sd90239)
  * @param onOauthSuccess The callback for when the oauth is successful and we have the token. Usually you
  * would link the token to a user using the `userID`. This function has to return a promise that resolves
- * when you finish processing
+ * when you finish processing. It must resolve the promise with a user object
  *
  * @param expressApp - The express app that you want to create routes on
  * @param authStartPath - (optional) The path the user should go to to start authentication (default: /auth/dropbox)
@@ -68,6 +82,7 @@ export default ({
   successRedirect,
   failureRedirect
 }: AuthOpts): void => {
+  expressApp.use(passport.initialize());
   // set up passport authentication
   passport.use(
     new DropboxOAuth2Strategy(
@@ -78,15 +93,28 @@ export default ({
         callbackURL
       },
       async (accessToken, refreshToken, profile, done) => {
+        // we create a custom user object that's easier to read
+        const user = {
+          userId: profile.id,
+          email: profile.emails[0].value,
+          name: {
+            givenName: profile.name.givenName,
+            familyName: profile.name.familyName,
+            displayName: profile.displayName
+          },
+          // any other user details
+          profile
+        };
+
         // instead of using callback style we expect the success function to
-        // resolve a promise on completion
-        await onOauthSuccess({
+        // resolve a promise on completion with the user object (with possible
+        // metadata attached)
+        const mutatedUser = await onOauthSuccess({
           accessToken,
           refreshToken,
-          profile,
-          userId: profile.id
+          user
         });
-        done();
+        done(null, mutatedUser);
       }
     )
   );
@@ -94,13 +122,13 @@ export default ({
   expressApp.get(authStartPath, passport.authenticate("dropbox-oauth2"));
 
   // ex: http://localhost:8888/dropbox-callback becomes /dropbox-callback
-  const callbackPath = url.parse(callbackURL).pathname;
+  const callbackPath = url.parse(callbackURL).path;
   expressApp.get(
     callbackPath,
-    passport.authenticate("dropbox-oauth2", { failureRedirect }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect(successRedirect);
-    }
+    passport.authenticate("dropbox-oauth2", {
+      failureRedirect,
+      successRedirect,
+      session: false
+    })
   );
 };
